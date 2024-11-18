b599b7c94f91f07715ed6589d6a505f14c19985c
danielpatricio0022 <hadukens@hotmail.com>
2024-11-18 20:36:37 -0300

    created dto and doing service

--- src/main/java/org/rinha/repository/PersonRepository.java
+++ src/main/java/org/rinha/repository/PersonRepository.java
@@ -2,11 +2,14 @@ package org.rinha.repository;
 
 import org.rinha.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
 public interface PersonRepository extends JpaRepository<Person, UUID> {
 
-
+    @Query("SELECT COUNT(p) > 0 FROM Person p WHERE p.apelido = :apelido")
+    boolean existsByApelido(@Param("apelido") String apelido);
 
 }
--- src/main/java/org/rinha/service/PersonService.java
+++ src/main/java/org/rinha/service/PersonService.java
@@ -1,15 +1,22 @@
 package org.rinha.service;
-
-
 import org.rinha.model.PersonRqDTO;
 import org.rinha.model.PersonRsDTO;
+import org.rinha.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PersonService {
 
+    @Autowired
+    PersonRepository personRepository;
+
     public PersonRsDTO createPerson(PersonRqDTO personRqDTO) {
-        return personRqDTO;
+          var data = personRepository.existsByApelido(personRqDTO.getApelido());
+            if (data) {
+                throw new RuntimeException("Apelido já existe"); // TODO: criar exceção
+            }
+
     }
 
 }
