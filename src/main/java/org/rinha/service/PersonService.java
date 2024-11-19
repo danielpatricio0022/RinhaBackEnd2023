d7f725ec488c9c7338a1a8a62cc47c60b8b92017
danielpatricio0022 <hadukens@hotmail.com>
2024-11-19 13:07:05 -0300

    .

--- src/main/java/org/rinha/service/PersonService.java
+++ src/main/java/org/rinha/service/PersonService.java
@@ -1,10 +1,15 @@
 package org.rinha.service;
+import org.hibernate.mapping.List;
+import org.rinha.exception.UnprocessableEntity;
 import org.rinha.model.Person;
 import org.rinha.model.PersonRqDTO;
 import org.rinha.model.PersonRsDTO;
 import org.rinha.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class PersonService {
@@ -15,7 +20,7 @@ public class PersonService {
     public PersonRsDTO createPerson(PersonRqDTO personRqDTO) {
           var data = personRepository.existsByApelido(personRqDTO.getApelido());
             if (data) {
-                throw new RuntimeException("apelido already exists"); // 422 TODO: exception
+                throw new UnprocessableEntity("apelido already exists"); // 422 TODO: exception
             }
               var person = new Person(
                       personRqDTO.getApelido(),
@@ -34,6 +39,43 @@ public class PersonService {
                         response.getStack()
                 );
     }
+
+    public PersonRsDTO getAllPerson(PersonRqDTO personRqDTO){
+
+
+    }
+
+    public PersonRsDTO getPersonById(PersonRqDTO personRqDTO, UUID id){
+
+      boolean data = personRepository.existsById(id);
+          if(!data){
+              throw new UnprocessableEntity("not found"); // tratar
+          }
+
+            Optional<Person> personOpt = personRepository.findById(id);
+
+               Person person = personOpt.orElseThrow
+                                          (RuntimeException::new);
+              // is present
+
+                return new PersonRsDTO(
+                        person.getUuid(),
+                        person.getApelido(),
+                        person.getNome(),
+                        person.getNascimento(),
+                        person.getStack()
+                );
+    }
+
+
+
+
+
+
+
+
+    }
+
 
 
 }
