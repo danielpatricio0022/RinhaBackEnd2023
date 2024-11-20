73bded7e1caf8bbe93672a63229a3c3b0efb12b1
danielpatricio0022 <hadukens@hotmail.com>
2024-11-20 16:46:07 -0300

    jakarta validation working, version altered

--- src/main/java/org/rinha/model/PersonRqDTO.java
+++ src/main/java/org/rinha/model/PersonRqDTO.java
@@ -1,8 +1,13 @@
 package org.rinha.model;
 
-import javax.validation.constraints.*;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDate;
 import java.util.List;
+
 
 public class PersonRqDTO {
 
